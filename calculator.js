bd9a5f039250c73441e2c4ae68cbd6d8e18ecdb2
devhamzat <ayomidelegbede@gmail.com>
2023-08-19 09:29:38 +0100

    calculator push

---
+++ calculator.js
@@ -0,0 +1,21 @@
+var display = document.getElementById("input-text");
+var error = document.getElementById("input-text");
+function displayFunction(number){
+    display.value += number;
+}
+
+function clearFunction(){
+    display.value = " "
+}
+function deleteFunction(){
+    display.value = display.value.slice(0,-1);
+}
+function calculate(){
+    try {
+        display.value = eval(display.value);        
+    } catch (error) {
+    
+       display.value = "Invalid input"
+    }
+    
+}
