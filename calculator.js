914346372288ca68e3804b607cffaafa061a29e8
devhamzat <ayomidelegbede@gmail.com>
2023-08-23 02:51:46 +0100

    upgraded the functionality

--- calculator.js
+++ calculator.js
@@ -1,20 +1,39 @@
-var display = document.getElementById("input-text");
-function displayFunction(number){
-    display.value += number;
+var errorMessage = "syntax error"
+function displayFunction(displayValue){
+    var display = document.getElementById("input-text");
+    if(displayValue === '.' && (display.value.endsWith('.')|| display.value ==='')){
+        return "";
+    }
+    if(displayValue === '+' && display.value.includes('+')){
+        return "";
+    }
+    if(displayValue === '-' && display.value.includes('-')){
+        return "";
+    }
+    if(displayValue === '*' && display.value.includes('*')){
+        return "";
+    }if(displayValue === '/' && display.value.includes('/')){
+        return "";
+    }
+    display.value += displayValue
 }
-
-function clearFunction(){
-    display.value = " "
-}
+function calcButton(){
+    var display = document.getElementById("input-text");
+    try{
+        display.value = eval(display.value);
+    }catch(error){
+        alert("syntax error ")
+    }
+    }
+   
 function deleteFunction(){
+    var display = document.getElementById("input-text");
     display.value = display.value.slice(0,-1);
 }
-function calculate(){
-    try {
-        display.value = eval(display.value);        
-    } catch (error) {
-    
-       display.value = "Invalid input"
-    }
-    
+function clearFunction(){
+    var display = document.getElementById("input-text");
+    display.value = "";
 }
+
+
+
