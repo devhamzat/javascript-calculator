3175a35b0d0202a6dca8a1e923df01cd32971803
devhamzat <ayomidelegbede@gmail.com>
2023-08-24 09:21:54 +0100

    change operator enabled

--- calculator.js
+++ calculator.js
@@ -1,33 +1,45 @@
-var errorMessage = "syntax error"
-function displayFunction(displayValue){
-    var display = document.getElementById("input-text");
-    if(displayValue === '.' && (display.value.endsWith('.')|| display.value ==='') || displayValue === '+' && display.value.includes('+')||displayValue === '-' && display.value.includes('-')||displayValue === '*' && display.value.includes('*')|| displayValue === '/' && display.value.includes('/')){
-        return "";
-    }
-    if ((displayValue === '+' || displayValue === '-' || displayValue === '*' || displayValue === '/') &&
-    (display.value.endsWith('+') || display.value.endsWith('-') || display.value.endsWith('*') || display.value.endsWith('/'))) {
-    return; 
-}
-    display.value += displayValue
-}
-function calcButton(){
-    var display = document.getElementById("input-text");
-    try{
-        display.value = eval(display.value);
-    }catch(error){
-        alert(errorMessage);
-    }
-    }
-   
-function deleteFunction(){
-    var display = document.getElementById("input-text");
-    display.value = display.value.slice(0,-1);
-}
-function clearFunction(){
-    var display = document.getElementById("input-text");
-    display.value = "";
+var errorMessage = "syntax error";
+var currentOperator = '';
+
+function changeOperator(newOperator) {
+  currentOperator = newOperator;
+  var display = document.getElementById("input-text");
+  if (display.value.endsWith('+') || display.value.endsWith('-') || display.value.endsWith('*') || display.value.endsWith('/')) {
+    display.value = display.value.slice(0, -1);
+  }
+  display.value += currentOperator;
 }
 
+function displayFunction(displayValue) {
+  var display = document.getElementById("input-text");
+  if ((displayValue === '+' || displayValue === '-' || displayValue === '*' || displayValue === '/') &&
+      (display.value.endsWith('+') || display.value.endsWith('-') || display.value.endsWith('*') || display.value.endsWith('/'))) {
+    return;
+  }
+  if (displayValue === '.' && (display.value.endsWith('.') || display.value === '')) {
+    return;
+  }
+  display.value += displayValue;
+}
+
+function calcButton() {
+  var display = document.getElementById("input-text");
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    alert(errorMessage);
+  }
+}
+
+function deleteFunction() {
+  var display = document.getElementById("input-text");
+  display.value = display.value.slice(0, -1);
+}
+
+function clearFunction() {
+  var display = document.getElementById("input-text");
+  display.value = "";
+}
 
 
 
